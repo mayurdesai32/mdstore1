5f6d7ff52f7153014a63bda0e0f08106b0e8a4da
mayur desai <msdesai32@gmail.com>
2023-09-04 07:37:08 +0530

    added docker file

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
-const DB_URI = process.env.DB_URI;
+const DB_CONTAINERNAME = process.env.DB_CONTAINERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_USERNAME = process.env.DB_USERNAME;
+
+// IF we are using docker
+
+let DB_URI;
+// DB_URI = `mongodb:${DB_USERNAME}@${DB_PASSWORD}:${DB_CONTAINERNAME}:27017/mdstore?authSource=admin`;
+if (process.env.DOCKER) {
+  DB_URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_CONTAINERNAME}:27017/mdstore?authSource=admin`;
+} else {
+  DB_URI = `${process.env.MONGODB_URI}`;
+}
+console.log(DB_URI);
 const db = async () => {
   try {
     await mongoose.connect(DB_URI, {
@@ -8,6 +21,7 @@ const db = async () => {
       useFindAndModify: false,
       useCreateIndex: true,
     });
+    // await mongoose.connect(DB_URI);
     console.log('database connected');
   } catch (error) {
     console.log('error occur on databate side ' + error);
